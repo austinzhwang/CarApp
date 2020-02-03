014f7987fdd13cb083d9fa0e61bc34599f843f34
Austin Wang <austinzhwang@gmail.com>
2020-02-03 16:04:38 -0500

    added abstract classes

---
+++ src/Boat.java
@@ -0,0 +1,18 @@
+public final class Boat {
+
+    public Boat() {
+
+    }
+
+    public String start() {
+        return "is starting";
+    }
+
+    public String accelerate() {
+        return "is accelerating";
+    }
+
+    public String stop() {
+        return "is stopped";
+    }
+}
---
+++ src/Plane.java
@@ -0,0 +1,23 @@
+public final class Plane {
+
+    public Plane() {
+
+    }
+
+    public String start() {
+        return "is starting";
+    }
+
+    public String accelerate() {
+        return "is accelerating";
+    }
+
+    public String stop() {
+        return "is stopped";
+    }
+
+    public String fly() {
+        return "is flying";
+    }
+
+}
